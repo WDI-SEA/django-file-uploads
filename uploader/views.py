439884db6f2a0f14b0f5f4d9746372ca5221aaf4
Steve Geluso <stevegeluso@gmail.com>
2017-02-16 14:20:19 -0800

    configuring form to upload file, and route to get file

--- uploader/views.py
+++ uploader/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-  return render(request, 'uploader/index.html')
+  if request.method == "GET":
+    return render(request, 'uploader/index.html')
+  if request.method == "POST":
+    srt_file = request.FILES['srt_file']
+    handle_uploaded_file(srt_file)
+    return redirect('index')
+  
+def handle_uploaded_file(file):
+  with open('out_test', 'wb+') as destination:
+    for chunk in file.chunks():
+      destination.write(chunk)
