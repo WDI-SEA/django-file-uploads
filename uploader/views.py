83c9af151633b1f1a83135dc91cf2ec7772812a9
Steve Geluso <stevegeluso@gmail.com>
2017-02-16 15:05:29 -0800

    processing uploaded file

--- uploader/views.py
+++ uploader/views.py
@@ -10,6 +10,45 @@ def index(request):
     return redirect('index')
   
 def handle_uploaded_file(file):
-  with open('out_test', 'wb+') as destination:
+  uploaded_filename = "out_test"
+  # read the uploaded file and write it to disk.
+  with open(uploaded_filename, 'wb+') as destination:
     for chunk in file.chunks():
       destination.write(chunk)
+      
+  # open the file that we just write to disk and
+  # send it to processing
+  ffile = open(uploaded_filename)
+  parse_subtitles(ffile)
+
+def parse_subtitles(subtitles):
+  print("var SUBTITLES = [")
+  while True:
+    line = subtitles.readline()
+    if line == '':
+      break
+    line_number = int(line)
+    time = subtitles.readline()
+    line1 = ""
+    line2 = subtitles.readline()
+    while True:
+      line = subtitles.readline()
+      if line and len(line) > 1:
+        line1 = line2
+        line2 = line
+      else:
+        break
+
+    time = time.strip()
+    line1 = line1.strip()
+    line2 = line2.strip()
+
+    line1 = line1.replace('"', "'")
+    line2 = line2.replace('"', "'")
+
+    print("{")
+    print("  duration: \"%s\"," % time)
+    print("  line1: \"%s\"," % line1)
+    print("  line2: \"%s\"," % line2)
+    print("},")
+  print("];")
